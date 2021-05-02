9395ca415baa32fc7a627aa265fe8278b0ce01a8
fengkx <liangkx8237@gmail.com>
2021-05-02 20:32:35 +0800

    fix: disable cache for ssr data in service worker

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
-const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 module.exports = withBundleAnalyzer(
   withPWA({
     webpack: (config) => {
-      if (process.env.ANALYZE === 'true') {
+      if (process.env.ANALYZE === "true") {
         config.plugins.push(new DuplicatePackageCheckerPlugin());
       }
 
       return config;
     },
     future: {
-      webpack5: true
+      webpack5: true,
     },
     pwa: {
-      dest: 'public'
-    }
+      dest: "public",
+      runtimeCaching: require("./pwa-runtime-cache"),
+    },
   })
 );
